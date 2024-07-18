112fce3525ccdff11308032250cf9bb456cdd859
LucasIbanez13 <lucaas00129@gmail.com>
2024-07-18 02:53:07 -0300

    agrego components

---
+++ src/Componente/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+      <footer className="sticky-footer bg-white">
+      <div className="container my-auto">
+        <div className="copyright text-center my-auto">
+          <span>Copyright &copy;  2023 | Full Gamer  </span>
+        </div>
+      </div>
+    </footer>
+    )
+}
